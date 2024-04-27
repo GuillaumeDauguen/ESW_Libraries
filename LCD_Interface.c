10243d27c71b2e9978d667ebe27a965cb7ff4531
Guillaume <dauguen.guillaume@gmail.com>
2024-04-27 15:23:58 +0200

    make LCD_Interface usable

--- LCD_Interface.c
+++ LCD_Interface.c
@@ -1,22 +1,20 @@
 /**
  ******************************************************************************
  * @file LCD_Interface.c
- * @brief "file" implementation file
+ * @brief LCD Interface implementation file
  *        Do things
  *
  * @creation 2024/04/24
- * @edition 2024/04/24
+ * @edition 2024/04/27
  * 
  * @author Guillaume Dauguen
  *
  ******************************************************************************
+ * @todo read busy flag
+ * @todo switch to interrupt mode
+ ******************************************************************************
  */
-#include <LCD_Interface.h>
-#include "main.h"
-
-// ------------------------------------------------------------------------
-// -------------------------------- MACROS --------------------------------
-// ------------------------------------------------------------------------
+#include "../ESW_Libraries/LCD_Interface.h"
 
 // ------------------------------------------------------------------------
 // ----------------------------- STATIC TYPES -----------------------------
@@ -24,41 +22,97 @@
 typedef enum {
 	INSTRUCTION = GPIO_PIN_RESET,
 	DATA = GPIO_PIN_SET,
-}register_select;
+} register_select;
 
 // ------------------------------------------------------------------------
 // -------------------------- STATIC PROTOTYPES ---------------------------
 // ------------------------------------------------------------------------
+static LCD_data *s_LCD;
 
 // ------------------------------------------------------------------------
 // ---------------------- STATIC FUCTIONS PROTOTYPES ----------------------
 // ------------------------------------------------------------------------
 static void _SendByte(register_select RS, uint8_t data);
 static void _SendHalfByte(register_select RS, uint8_t data);
+static void _SetPinState(GPIO gpio, GPIO_PinState PinState);
 
 // ------------------------------------------------------------------------
 // ------------------- PUBLIC FUNCTIONS IMPLEMENTATION --------------------
 // ------------------------------------------------------------------------
-void LCD_Interface_Init() {
+void LCD_Interface_Init(LCD_data *LCD) {
+	s_LCD = LCD;
+	s_LCD->cursor_row = 0;
+	s_LCD->cursor_position = 0;
+
 	HAL_Delay(50);
-	_SendHalfByte(INSTRUCTION, 0b0011);	//
-	_SendHalfByte(INSTRUCTION, 0b0011);	// Manual Reset
-	_SendHalfByte(INSTRUCTION, 0b0011);	//
+	LCD_Interface_Reset();
 
 	_SendHalfByte(INSTRUCTION, 0b0010);	// 4-bit comm
-	_SendByte(INSTRUCTION, 0b00101000);	// 2 lines
-	_SendByte(INSTRUCTION, 0b00000001);	// clear screen
-	_SendByte(INSTRUCTION, 0b00000010);	// return home
-	_SendByte(INSTRUCTION, 0b00001110);
-	_SendByte(INSTRUCTION, 0b00000110);
+	LCD_Interface_SendInstruction(0b00101000);	// Function Set: set Data length (4-bit bus), line number (2 lines), font (5x8 dots)
+	LCD_Interface_SendInstruction(0b00000001);	// Clear display
+	LCD_Interface_SendInstruction(0b00000010);	// Return home
+	LCD_Interface_SendInstruction(0b00001100);	// Display ON/OFF: set display (on), cursor (off), cursor blink (off)
+	LCD_Interface_SendInstruction(0b00000110);	// Entry Mode Set: set the moving direction of cursor (right), display (no shift)
 }
 
-void LCD_Interface_Run() {
-	char hello[] = "Hello, world !";
-	for (int i = 0; i < 14; ++i) {
-		_SendByte(DATA, hello[i]);
+void LCD_Interface_Reset() {
+	// 40ms min delay after power up
+	_SendHalfByte(INSTRUCTION, 0b0011);
+	// 4.1ms min delay
+	_SendHalfByte(INSTRUCTION, 0b0011);
+	// 100us min delay
+	_SendHalfByte(INSTRUCTION, 0b0011);
+}
+
+void LCD_Interface_Home() {
+	LCD_Interface_SendInstruction(0b00000010);	// return home
+	s_LCD->cursor_row = 0;
+	s_LCD->cursor_position = 0;
+}
+
+void LCD_Interface_SetCursorPos(uint8_t row, uint8_t col) {
+	LCD_Interface_SendInstruction(0x80 | (row << 6) | col);
+	s_LCD->cursor_row = row;
+	s_LCD->cursor_position = col;
+}
+
+void LCD_Interface_PrintString(uint8_t *data, uint8_t len) {
+	for (uint8_t i = 0; i < len; i++) {
+		LCD_Interface_PrintChar(data[i]);
 	}
-	_SendByte(INSTRUCTION, 0b00000010);	// return home
+}
+
+void LCD_Interface_PrintChar(uint8_t data) {
+	LCD_Interface_SendData(data);
+	s_LCD->cursor_position++;
+	if (s_LCD->cursor_position >= s_LCD->memory_cols) {
+		s_LCD->cursor_position %= s_LCD->memory_cols;
+		LCD_Interface_SetCursorPos(s_LCD->cursor_row, s_LCD->cursor_position);
+	}
+}
+
+void LCD_Interface_SendData(uint8_t data) {
+	_SendByte(DATA, data);
+}
+
+void LCD_Interface_SendInstruction(uint8_t data) {
+	_SendByte(INSTRUCTION, data);
+}
+
+/*
+ * CGRAM addr [0:7]
+ * Need a 8 bytes array
+ */
+void LCD_Interface_StorePattern(uint8_t CGRAL_addr, uint8_t *pattern) {
+	LCD_Interface_SendInstruction(0x40 | ((CGRAL_addr & 0x07) << 3));
+	for (uint8_t i = 0; i < 8; i++) {
+		_SendByte(DATA, pattern[i]);
+	}
+	LCD_Interface_SetCursorPos(s_LCD->cursor_row, s_LCD->cursor_position);
+}
+
+void LCD_Interface_Shift(LCD_ELEMENT element, LCD_DIRECTION dir) {
+	_SendByte(INSTRUCTION, (0x10 | (element << 3) | (dir << 2)));
 }
 
 // ------------------------------------------------------------------------
@@ -70,19 +124,23 @@ void _SendByte(register_select RS, uint8_t data) {
 }
 
 void _SendHalfByte(register_select RS, uint8_t data) {
-	HAL_GPIO_WritePin(LCD_RS_GPIO_Port, LCD_RS_Pin, RS);
-	HAL_GPIO_WritePin(LCD_RW_GPIO_Port, LCD_RW_Pin, GPIO_PIN_RESET);
+	_SetPinState(s_LCD->RS, RS);
+	_SetPinState(s_LCD->RW, GPIO_PIN_RESET);
 
-	HAL_GPIO_WritePin(LCD_D7_GPIO_Port, LCD_D7_Pin, (data & (1 << 3)));
-	HAL_GPIO_WritePin(LCD_D6_GPIO_Port, LCD_D6_Pin, (data & (1 << 2)));
-	HAL_GPIO_WritePin(LCD_D5_GPIO_Port, LCD_D5_Pin, (data & (1 << 1)));
-	HAL_GPIO_WritePin(LCD_D4_GPIO_Port, LCD_D4_Pin, (data & (1 << 0)));
+	_SetPinState(s_LCD->D7, (data & (1 << 3)));
+	_SetPinState(s_LCD->D6, (data & (1 << 2)));
+	_SetPinState(s_LCD->D5, (data & (1 << 1)));
+	_SetPinState(s_LCD->D4, (data & (1 << 0)));
 
 	HAL_Delay(1);
-	HAL_GPIO_WritePin(LCD_Enable_GPIO_Port, LCD_Enable_Pin, GPIO_PIN_SET);
+	_SetPinState(s_LCD->EN, GPIO_PIN_SET);
 	HAL_Delay(1);
-	HAL_GPIO_WritePin(LCD_Enable_GPIO_Port, LCD_Enable_Pin, GPIO_PIN_RESET);
+	_SetPinState(s_LCD->EN, GPIO_PIN_RESET);
 	HAL_Delay(1);
+}
+
+void _SetPinState(GPIO gpio, GPIO_PinState PinState) {
+	HAL_GPIO_WritePin(gpio.Port, gpio.Pin, PinState);
 }
 
 
--- LCD_Interface.h
+++ LCD_Interface.h
@@ -1,11 +1,11 @@
 /**
  ******************************************************************************
- * @file 1602_Interface.h
- * @brief "file" implementation file
+ * @file LCD_Interface.h
+ * @brief LCD Interface implementation file
  *        Do things
  *
  * @creation 2024/04/24
- * @edition 2024/04/24
+ * @edition 2024/04/27
  * 
  * @author Guillaume Dauguen
  *
@@ -14,14 +14,58 @@
 #ifndef __1602_INTERFACE_H__
 #define __1602_INTERFACE_H__
 
+#include "main.h"
+
+#include <stdint.h>
+
 // ------------------------------------------------------------------------
 // ----------------------------- PUBLIC TYPES -----------------------------
 // ------------------------------------------------------------------------
+typedef struct {
+		GPIO_TypeDef *Port;
+		uint16_t Pin;
+} GPIO;
+
+typedef struct {
+		uint8_t rows;
+		uint8_t display_cols;
+		uint8_t memory_cols;
+
+		uint8_t cursor_row;
+		uint8_t cursor_position;
+
+		GPIO RS;
+		GPIO RW;
+		GPIO EN;
+		GPIO D7;
+		GPIO D6;
+		GPIO D5;
+		GPIO D4;
+} LCD_data;
+
+typedef enum {
+	CURSOR = 0,
+	DISPLAY = 1,
+} LCD_ELEMENT;
+
+typedef enum {
+	LEFT = 0,
+	RIGHT = 1,
+} LCD_DIRECTION;
 
 // ------------------------------------------------------------------------
 // ------------------- PUBLIC FUNCTIONS IMPLEMENTATION --------------------
 // ------------------------------------------------------------------------
-void LCD_Interface_Init();
+void LCD_Interface_Init(LCD_data *LCD);
+void LCD_Interface_Reset();
+void LCD_Interface_Home();
+void LCD_Interface_SetCursorPos(uint8_t row, uint8_t col);
+void LCD_Interface_SendInstruction(uint8_t data);
+void LCD_Interface_SendData(uint8_t data);
+void LCD_Interface_PrintChar(uint8_t data);
+void LCD_Interface_PrintString(uint8_t *data, uint8_t len);
+void LCD_Interface_Shift(LCD_ELEMENT element, LCD_DIRECTION dir);
+void LCD_Interface_StorePattern(uint8_t CGRAM_addr, uint8_t *pattern);
 void LCD_Interface_Run();
 
 #endif /* __1602_INTERFACE_H__ */
